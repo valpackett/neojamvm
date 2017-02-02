2b614e5cbd8dfaa49de5e0b585b69254a04439e6
Val Packett <val@packett.cool>
2017-02-02 22:54:55 +0300

    add some bsd fixes

--- src/os/bsd/os.c
+++ src/os/bsd/os.c
@@ -97,3 +97,18 @@ char *nativeLibMapName(char *name) {
 long long nativePhysicalMemory() {
     return 0; /* TBD */
 }
+
+char *nativeJVMPath() {
+    Dl_info info;
+    char *path;
+
+    if(dladdr(nativeJVMPath, &info) == 0) {
+        printf("Error: dladdr failed.  Aborting VM\n");
+        exitVM(1);
+    }
+
+    path = sysMalloc(strlen(info.dli_fname) + 1);
+    strcpy(path, info.dli_fname);
+
+    return path;
+}
